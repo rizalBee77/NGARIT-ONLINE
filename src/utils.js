9c97046d863bcccbd80ebfeca50645fa57550493
0xRizalxyz <72789671+rizalBee77@users.noreply.github.com>
2024-11-27 18:07:15 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,11 +5,11 @@ const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 function displayHeader() {
     process.stdout.write('\x1Bc');
     console.log('========================================'.cyan);
-    console.log('=        Grass Airdrop Bot - V2        ='.cyan);
-    console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-    console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
-    console.log('========================================'.cyan);
-    console.log();
-  }
+  console.log('=               GRASS BOT              ='.cyan);
+  console.log('=           Coding by 0xRizal          ='.cyan);
+  console.log('=          Only For P2E Member         ='.cyan);
+  console.log('========================================'.cyan);
+  console.log();
+}
 
 module.exports = { delay, displayHeader };
